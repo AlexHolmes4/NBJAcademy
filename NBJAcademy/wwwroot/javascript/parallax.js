64fa2c0ed1c794c92bce0486c6349d5b0cbd8bf0
Alex Holmes <00113969@uwa.edu.au>
2025-08-14 21:36:59 +0800

    testing different backgrounds

---
+++ NBJAcademy/wwwroot/javascript/parallax.js
@@ -0,0 +1,7 @@
+﻿window.setParallax = function (selector) {
+    const el = document.querySelector(selector);
+    window.addEventListener('scroll', function () {
+        let offset = window.pageYOffset;
+        el.style.backgroundPositionY = offset * 0.2 + "px";
+    });
+}
