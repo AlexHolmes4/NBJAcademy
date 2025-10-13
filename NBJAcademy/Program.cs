23ff71cfd3b9d00be3340f01d7ddb5444ac4fa51
Alex Holmes <00113969@uwa.edu.au>
2025-10-13 09:13:40 +0800

    wip

--- NBJAcademy/Program.cs
+++ NBJAcademy/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NBJAcademy;
+using NBJAcademy.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped<ScreenSizeService>();
 
 await builder.Build().RunAsync();
